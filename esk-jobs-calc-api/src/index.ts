4859ae7f0b989a22f516d52891cb4534ac75cadd
Edson Shideki Kokado <eskokado@gmail.com>
2021-04-08 20:25:55 -0300

    add esk-jobs-calc-api

---
+++ esk-jobs-calc-api/src/index.ts
@@ -0,0 +1,20 @@
+﻿import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import bodyParser from "body-parser";
+import { useRoutes } from "./routes";
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+app.use(bodyParser.json());
+useRoutes(app);
+
+app.get("/", (req, res) => {
+  res.json({
+    msg: "OK",
+  });
+});
+
+app.listen(PORT, () => console.log("Servidor iniciado na porta " + PORT));
